9dc9a6962520905cb38bc446a604471d2e2e7742
littleflower0260-cmd <littleflower0260@gmail.com>
2025-09-06 20:01:03 +0530

    Delete Script.js
    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-// Basic structure for now
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("School App Loaded");
-
-  // Future: Supabase integration, login, calendar fetch, notices update etc.
-});
